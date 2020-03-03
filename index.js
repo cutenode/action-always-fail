4de12f604346075f9525a534849eece6b929323c
Tierney Cyren <hello@bnb.im>
2020-03-03 12:11:25 -0500

    feat: inital "working" commit

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require('@actions/core')
 
 // most @actions toolkit packages have async methods
-async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
+async function action () {
+  core.setFailed('🚨 Failed because this action always fails.\n\nIf you want it to stop failing, remove this action.')
 }
 
-run()
+action()
